9031e998b7ef58108ab244c73efdc788c7df93c4
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-03-19 19:33:45 +0530

    19March

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,21 +1,23 @@
-import {useEffect} from "react";
-
-
-
-
+import {useEffect , useState} from "react";
+import styled from "styled-components";
+import {Splide,SplideSlide} from '@splidejs/react-splide';
+// USed for React Carousel  
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
   
+  const [popular,setPopular] = useState([]);
+
+
   useEffect(()=>{
     getPopular();
-  },[])
+  },[]);
 
     const getPopular= async() =>
     {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`)
       const data = await api.json();
-      console.log(data);
-    
+      setPopular(data.recipes)
     
     };
 
@@ -24,10 +26,60 @@ function Popular() {
 
     return (
     <div>
-        Popular
+     <Wrapper>
+       <h3>Popular Picks</h3>
+        <Splide 
+        options={{
+          perPage: 4,
+          arrows:false,
+          pagination:false,
+          drag:'free',
+          gap:'5rem',
+
+        }}>
+          
+            {popular.map((recipe)=>{
+              return(
+                <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.titile} />
+                </Card>
+                </SplideSlide>
+                );
+              })}
+              </Splide>
+            </Wrapper>   
     </div>
       )
  
 }
 
-export default Popular
+const Wrapper = styled.div`
+margin: 4rem 0rem ;
+`;
+
+const Card = styled.div`
+min-height:25rem;
+border-radius:2rem;
+overflow:hidden;
+position:relative;
+ 
+img{
+  border-radius:2rem;
+  position : absolute;
+  left:0;
+  width:100%;
+  height:100%;
+  object-fit:cover;
+}
+
+p{
+  
+}
+
+
+
+`;
+
+export default Popular;
