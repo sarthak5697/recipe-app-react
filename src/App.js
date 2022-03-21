4b12a282d427f882a040640b7ea3006e5c6be78f
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-03-21 20:43:17 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import {BrowserRouter} from 'react-router-dom';
+import Search from "./components/Search";
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Search/>
       <Category/>
       <Pages/>
       </BrowserRouter>
