24b46591929fb57ae2a004171ca34b5d45f0a149
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-03-23 13:43:57 +0530

    c3

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,21 +1,25 @@
 import Home from "./Home";
 import React from 'react';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route, useLocation } from "react-router-dom";
 import Recipe from "./Recipe";
 import Cuisine from "./Cuisine";
 import Searched from "../pages/Searched";
-
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+
+  const location = useLocation();
   return (
-      <Routes>
+     <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.path}>
         <Route path="/" element={<Home/>} />
         <Route path="/cuisine/:type" element={<Cuisine/>} />
         <Route path="/searched/:search" element={<Searched/>} />
         <Route path="/recipe/:name" element={<Recipe/>} />
       </Routes>
+     </AnimatePresence>
   
-  )
+  );
 }
 
 export default Pages
